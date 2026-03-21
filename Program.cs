e59a1069c838e04ad4c617562583d9e69722ba71
BUDIWAHYU\herlenadita <budiwahyuherlenadita@gmail.com>
2026-03-21 15:55:12 +0700

    validation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using PatientReferralManagementAPI.Data;
 using PatientReferralManagementAPI.Helpers;
-using PatientReferralManagementAPI.Mapping;
 using PatientReferralManagementAPI.Repositories;
 using PatientReferralManagementAPI.Services;
+using PatientReferralManagementAPI.Validators;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,10 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamingPolicy();
         options.JsonSerializerOptions.DictionaryKeyPolicy = new SnakeCaseNamingPolicy();
     });
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateUpdatePatientValidator>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
---
+++ Validators/CreateUpdatePatientValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using PatientReferralManagementAPI.DTO.Patient;
+namespace PatientReferralManagementAPI.Validators
+{
+    public class CreateUpdatePatientValidator : AbstractValidator<CreateUpdatePatientDto>
+    {
+        /**
+         * TODO
+         * - FirstName and LastName should only contain letters and spaces, and be between 2 and 200 characters long.
+         * - DateOfBirth should be a valid date not more than today's date.
+         */
+        public CreateUpdatePatientValidator()
+        {
+            // FirstName
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required")
+                .Length(2, 200).WithMessage("First name must be between 2 and 200 characters")
+                .Matches(@"^[a-zA-Z\s]+$").WithMessage("First name can only contain letters and spaces");
+
+            // LastName
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required")
+                .Length(2, 200).WithMessage("Last name must be between 2 and 200 characters")
+                .Matches(@"^[a-zA-Z\s]+$").WithMessage("Last name can only contain letters and spaces");
+
+            // DateOfBirth
+            RuleFor(x => x.DateOfBirth)
+                .NotEmpty().WithMessage("Date of birth is required")
+                .Must(d => d.Date <= DateTime.Today)
+                .WithMessage("Date of birth cannot be in the future");
+        }
+
+    }
+}
